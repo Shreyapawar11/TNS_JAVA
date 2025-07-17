d46542a6374a6889dffb501ac687a2b2e83fba99
Shreya <shreyapawar811@gmail.com>
2025-07-18 01:36:57 +0530

    Initial commit

---
+++ FirstProgram/src/com/FirstProgram/Demo.java
@@ -0,0 +1,15 @@
+package com.FirstProgram;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello World" + " " + 12);
+		
+		System.out.println("Java Programming");
+		
+		System.out.print(234 + 10);
+
+	}
+
+}
