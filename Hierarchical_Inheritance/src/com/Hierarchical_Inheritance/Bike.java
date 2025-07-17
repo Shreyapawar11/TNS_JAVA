a5d8350dc6925c9d2f56a96e8cb80057ea9294ea
Shreya <shreyapawar811@gmail.com>
2025-07-18 01:37:53 +0530

    Initial commit

---
+++ Hierarchical_Inheritance/src/com/Hierarchical_Inheritance/Bike.java
@@ -0,0 +1,10 @@
+package com.Hierarchical_Inheritance;
+
+public class Bike extends Vehicle{
+	String handleDesign;
+
+	public void twoWheeler()
+	{
+		System.out.println("Bike has two wheelers");
+	}
+}
---
+++ Hierarchical_Inheritance/src/com/Hierarchical_Inheritance/Demo.java
@@ -0,0 +1,17 @@
+package com.Hierarchical_Inheritance;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Bike b = new Bike();
+		b.engine();
+		b.twoWheeler();
+		
+		Car c = new Car();
+		c.speed();
+		c.fuelType();
+	}
+
+}
