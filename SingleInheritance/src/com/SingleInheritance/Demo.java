9dcd420c3134a891324b3b697309814812d10fb7
Shreya <shreyapawar811@gmail.com>
2025-07-18 01:44:38 +0530

    Initial commit

---
+++ SingleInheritance/src/com/SingleInheritance/Demo.java
@@ -0,0 +1,12 @@
+package com.SingleInheritance;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Student stu = new Student("Gayatri", 675547823456L, "Pune",9876543212L,101,"JSPM College");
+		
+		System.out.println(stu);
+	}
+
+}
