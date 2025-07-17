34daa341b78d7ac3e73047aaa5266bce0faaf84c
Shreya <shreyapawar811@gmail.com>
2025-07-18 01:28:30 +0530

    Initial commit

---
+++ AccessModifiers/src/com/AccessModifiers/Executor.java
@@ -0,0 +1,15 @@
+package com.AccessModifiers;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Base b = new Base();
+		
+		b.methodDefault();
+		b.methodProtected();
+		b.methodPublic();
+				
+
+}
+}
